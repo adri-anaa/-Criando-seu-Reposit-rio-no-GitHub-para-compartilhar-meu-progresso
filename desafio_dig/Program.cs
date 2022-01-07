f3786ceb8c2b6552e8e8a03c0bd098ebd91353e1
adri-anaa <adrialmeid@hotmail.com>
2022-01-07 13:02:04 -0300

    desafio

---
+++ desafio_dig/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace desafio_dig
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            var testes = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < testes; i++)
+
+            {
+
+                var esferas = int.Parse(Console.ReadLine());
+
+
+                Console.WriteLine(esferas - Math.Floor(Math.Sqrt(esferas)));
+
+            }
+
+        }
+
+    }
+
+}
+
+
